c7c63a157dd1efa1793bcb0dc56965157fb60334
lukelovekin@gmail.com <lukelovekin@gmail.com>
2024-10-28 07:37:54 +1000

    clean up tomtom client code

--- src/clients/tomtom/tomtom-client.ts
+++ src/clients/tomtom/tomtom-client.ts
@@ -1,6 +1,6 @@
 import { ConfigService } from '../../services/config-service/config-service';
 import { LoggerService } from '../../services/logger-service/logger-service';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { SearchOptions } from '../../../generated/maps-search';
 import { FuzzySearchRS, FuzzySearchResult } from '../../../generated/tomtom';
 
@@ -9,6 +9,35 @@ export class TomTomClient {
   constructor(
     private readonly config: ConfigService,
   ){}
+  
+   getFuzzySearchRS(address: string, options: SearchOptions): Promise<AxiosResponse<FuzzySearchRS>>{
+    const {apiBaseUrl, apiKey, apiVersion, fuzzySearchTimeout} = this.config
+
+    return axios({
+      method: 'get',
+      url: `${apiBaseUrl}/search/${apiVersion}/search/${address}.json`,
+      timeout: fuzzySearchTimeout,
+      params: {
+        key: apiKey,
+        limit: options.limit,
+        countrySet: options.countrySet,
+      },
+    });
+  }
+
+  checkFuzzySearchRS(searchData: FuzzySearchRS, status: number): void{
+    const internalServerErrorCodes = [400, 403, 404, 405, 596] 
+
+    const isExpectedRS = (searchData as FuzzySearchRS).results !== undefined
+
+    const isExternalServerError = status > 499 && status < 600 && !internalServerErrorCodes.includes(status)
+
+    if(!isExpectedRS) throw new Error(`Unexpected or Error response received for fuzzySearch: ${searchData}`);
+
+    if(internalServerErrorCodes.includes(status)) throw new Error('Internal Server Error')
+
+    if(isExternalServerError) throw new Error('External Server Error')
+  }
  
   async fuzzySearch(
     address: string,
@@ -22,30 +51,14 @@ export class TomTomClient {
 
     const tooManyRequestsStatusCode = 429
 
-    const internalServerErrorCodes = [400, 403, 404, 405, 596] 
-
     const noResults: FuzzySearchRS = { results: [] }
 
     while (retries < Math.max(fuzzySearchMaxRetries, 1)) { // iterate at least once
       try {
-        const {apiBaseUrl, apiKey, apiVersion, fuzzySearchTimeout} = this.config
 
-        const { data: searchData, status, headers } = await axios({
-          method: 'get',
-          url: `${apiBaseUrl}/search/${apiVersion}/search/${address}.json`,
-          timeout: fuzzySearchTimeout,
-          params: {
-            key: apiKey,
-            limit: options.limit,
-            countrySet: options.countrySet,
-          },
-        });
-
-        const isExpectedRS = (searchData as FuzzySearchRS).results !== undefined
-
-        if(!isExpectedRS) throw new Error(`Unexpected or Error response received for fuzzySearch: ${searchData}`);
-
-        const isExternalServerError = status > 499 && status < 600 && !internalServerErrorCodes.includes(status)
+        const { data: searchData, status, headers } = await this.getFuzzySearchRS(address, options)
+        
+        this.checkFuzzySearchRS(searchData, status)
 
         if(status === tooManyRequestsStatusCode) {
           const retryAfter = parseInt(headers['retry-after'], 10) || this.config.fuzzySearchRetryInSecs; // retry after x seconds if client server doesnt specify
@@ -53,10 +66,6 @@ export class TomTomClient {
           await new Promise(resolve => setTimeout(resolve, retryAfter * 1000)); // pause execution
           continue
         }
-
-        if(internalServerErrorCodes.includes(status)) throw new Error('Internal Server Error')
-
-        if(isExternalServerError) throw new Error('External Server Error')
 
         const filteredResults = searchData?.results?.filter((result: FuzzySearchResult) => {
           const hasMatchingCountryCode = result?.address?.countryCode === options.countrySet
